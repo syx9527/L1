024dbdf751e43ec496a479f3d33d09d9d811d9a9
syx <1275241305@qq.com>
2022-10-18 16:42:18 +0800

    ...

--- BigData_and_AI/main.py
+++ BigData_and_AI/main.py
@@ -2,7 +2,6 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import numpy as np
 
 
 def print_hi(name):
